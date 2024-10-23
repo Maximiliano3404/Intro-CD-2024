f7ed107a3c002d17e8019c28c289cc8ce66f12d3
Maximiliano3404 <maxrodria@gmail.com>
2024-10-23 12:55:50 -0600

    trabajo arrglado
    se cambio la variable a min

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def hello_world():
     return True
 
 def get_minimum(my_list):
-    im_value = my_list[0]
+    min_value = my_list[0]
     for element in my_list:
         if min_value >= element:
             min_value = element
