00b9aec4892aa845d75522d593985b798ccd8a32
Maximiliano3404 <maxrodria@gmail.com>
2024-10-23 12:46:29 -0600

    Trabajo
    agregamos nuevo codigo y borramos varias cosas de

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ def hello_world():
     print("Hello world")
     return True
 
+def get_minimum(my_list):
+    im_value = my_list[0]
+    for element in my_list:
+        if min_value >= element:
+            min_value = element
+    return min_value
+
 hello_world()
